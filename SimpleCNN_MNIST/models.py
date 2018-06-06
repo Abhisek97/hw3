d43e2e1e1fcde83ce37cdc10784f921ed303a188
Abhisek Singh <abs012@ieng6-202.ucsd.edu>
2018-06-06 15:09:37 -0700

    initial files

---
+++ SimpleCNN_MNIST/models.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class classifier(nn.Module):
+    def __init__(self, isDropOut = True):
+        super(classifier, self).__init__()
+        ## WRITE YOUR CODE HERE, Define the network structure ##
+
+    def forward(self, x):
+        ## WRITE YOUR CODE HERE ##
+
+        return x
+
