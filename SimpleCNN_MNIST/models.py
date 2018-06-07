06d7d246bc7f7bf4487b2d190ff79771e1d1f0a0
Abhisek Singh <abs012@its-cseb260-31.ucsd.edu>
2018-06-06 18:38:25 -0700

    print messages

--- SimpleCNN_MNIST/models.py
+++ SimpleCNN_MNIST/models.py
@@ -13,10 +13,12 @@ class classifier(nn.Module):
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        ## WRITE YOUR CODE HERE ##
+        print("X is " + x);
+        print("self.conv1 is " + self.conv1);
+        print("self.conv2 is " + self.conv2);
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        print("X is " + x);
+
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
