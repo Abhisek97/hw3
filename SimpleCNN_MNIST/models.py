d8f09b6a665ce64417a71e948c49fb52cb0270ff
Abhisek Singh <abs012@its-cseb260-31.ucsd.edu>
2018-06-06 18:31:00 -0700

    ree

--- SimpleCNN_MNIST/models.py
+++ SimpleCNN_MNIST/models.py
@@ -7,7 +7,7 @@ class classifier(nn.Module):
         super(classifier, self).__init__()
         self.conv1 = nn.Conv2d(5,32,3,padding=2,stride=2)
         self.pool = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(5,32,1,padding=2,stride=2)
+        self.conv2 = nn.Conv2d(5,64,1,padding=2,stride=2)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
@@ -16,6 +16,7 @@ class classifier(nn.Module):
         ## WRITE YOUR CODE HERE ##
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
+        print("X is " + x);
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
