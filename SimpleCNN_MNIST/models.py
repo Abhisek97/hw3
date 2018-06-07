e203c48c58046ba28aca63c54d9e37f4c249e02a
Abhisek Singh <abs012@its-cseb260-31.ucsd.edu>
2018-06-06 18:27:09 -0700

    added padding and strid

--- SimpleCNN_MNIST/models.py
+++ SimpleCNN_MNIST/models.py
@@ -5,9 +5,9 @@ import torch.nn.functional as F
 class classifier(nn.Module):
     def __init__(self, isDropOut = True):
         super(classifier, self).__init__()
-        self.conv1 = nn.Conv2d(5,32,3,3,1)
+        self.conv1 = nn.Conv2d(5,32,3,padding=2,stride=2)
         self.pool = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(5,32,1,1,1)
+        self.conv2 = nn.Conv2d(5,32,1,padding=2,stride=2)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
